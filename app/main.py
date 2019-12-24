be3648dc7c06fdb8bfcfd13869214c402a2e5601
Sukjun Sagong <ricepotato40@gmail.com>
2019-12-24 20:10:41 +0900

    log formatter, launch

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ from app.mod2 import mod_func as mod2_func
 import logging
 
 log = logging.getLogger("sample." + __name__)
-log.addHandler(logging.StreamHandler())
+stream_handler = logging.StreamHandler()
+format_str = "[%(levelname)s]|%(filename)s:%(lineno)s] %(asctime)s : %(message)s"
+formatter = logging.Formatter(format_str)
+stream_handler.setFormatter(formatter)
+log.addHandler(stream_handler)
 log.setLevel(logging.INFO)
 
 
