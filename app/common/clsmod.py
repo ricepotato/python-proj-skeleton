b2d1c7158cdfee99be6713cb3e9a5cf696522549
ricepotato <sukjun40@naver.com>
2020-01-05 01:36:05 +0900

    config

--- app/common/clsmod.py
+++ app/common/clsmod.py
@@ -1,10 +1,15 @@
 import logging
+import configparser
 
 log = logging.getLogger("sample." + __name__)
 
 
 class SomeClass(object):
     """ Test Some class """
+
+    def __init__(self):
+        self.cfg = configparser.ConfigParser()
+        self.cfg.read("conf/database.conf")
 
     def add(self, a, b):
         log.info(f"add ${a} + ${b} = ${a + b}")
--- tests/test_modcls.py
+++ tests/test_modcls.py
@@ -14,3 +14,6 @@ class ModClsTestCase(unittest.TestCase):
         res = self.obj.add(1, 2)
         assert res
         self.assertEqual(res, 3)
+
+    def test_cfg(self):
+        self.assertEqual(self.obj.cfg["somedb"]["db"], "somedb")
